ef6ee01589b8d2ee1a7a27992c090fece2b94da4
Ali Eskandari <aeskandari@snapchat.com>
2024-10-26 01:19:02 +0000

    more cleanup

--- packages/nextjs/bin/push.sh
+++
@@ -1,7 +0,0 @@
-export PROJECT_ID=engagement-management-dev
-export REPO_NAME=us.gcr.io
-export IMAGE_NAME=nextjs
-
-docker tag $IMAGE_NAME:latest us-docker.pkg.dev/$PROJECT_ID/$REPO_NAME/$IMAGE_NAME:latest
-
-docker push us-docker.pkg.dev/$PROJECT_ID/$REPO_NAME/$IMAGE_NAME:latest
