c917249805789d5f6eec2ee20f513e605db02f63
Ali Eskandari <aeskandari@snapchat.com>
2024-10-28 15:56:47 +0000

    added part 2

--- packages/nextjs/src/app/page.tsx
+++ packages/nextjs/src/app/page.tsx
@@ -14,8 +14,8 @@ Part 1: Fetching Events and Attendees
     3. Define a GraphQL query GET_EVENTS that fetches the id, title,
     and attendees.email fields for all events using the "events" query.
     
-    4. Fetch the events using the client.request method and set the
-    events in the state using setEvents
+    4. Fetch the events using the client.request method and store the events in 
+    the `events` state variable.
     
     5. Render the events and attendees. For each event, render the
     title and a list of attendees' emails in an unordered list.
@@ -26,6 +26,18 @@ Part 1: Fetching Events and Attendees
     Conference
         - bob@snap.com
         - charlie@snap.com
+*/
+
+/*
+Part 2: Add Attendees
+    1. Write a Graphql mutation to add an attendee using the 
+    `addAttendee` mutation.
+
+    2. Add code to capture the new attendee's email from the user
+    on the page.
+
+    3. Add a button that, when clicked, submits the email to the backend
+    using the mutation from step 1.
 */
 
 const GET_EVENTS = gql``;
